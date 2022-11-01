7d5838f7f49414c5b698cf03b5cdb37fd2c1c65d
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-11-02 00:44:54 +0900

    added random route

---
+++ docs/_app/immutable/chunks/chapter01-ec7d2cc0.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index-388cf647.js";import{b as o}from"./body-48bd83fc.js";const s=t(o,r=>{let e=Math.floor(r.length/5);return r.slice(0,e)});export{s as c};
---
+++ src/routes/random.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { body } from "$stores/body";
+
+  let verses, verse = 'where is my body'
+
+  body.subscribe(value => verses = value)
+
+  setInterval(() => {
+    let index = Math.floor(Math.random()*verses.length)
+    verse = verses[index]
+
+  }, 10000);
+  
+
+</script>
+
+<main class="px-6 flex flex-col justify-center align-center h-full w-full">
+  <p>{verse}</p>
+</main>
